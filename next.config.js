cff51473139286dbf878f674d7a7ebf74fbc5339
Woody <kazettique@gmail.com>
2021-06-06 20:57:01 +0800

    update deploy setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isProduction ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-  assetPrefix: isProduction ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
